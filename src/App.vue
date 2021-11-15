503b7953a46d7d8452bc253a92c68641163be056
Leonardo <ambrosioleonardo57@gmail.com>
2021-11-15 10:28:27 -0300

    title

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainComponent from './components/MainComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainComponent
   }
 }
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://i.imgur.com/3UePe8W.jpg')`
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Courier New;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+}
+  #avatar {
+    width: 200px;
+    height: 200px;
+    background: rgb(56, 56, 56);
+    border-radius: 300px;
+    position: relative;
+    transform: translate(-50%, 0);
+    background-size: 100%;
+    left: 50%;
+    border: 5px solid #fff;
+}
+
+.githubRedirect {
+  color: white;
+  font-size: 25px;
+  text-decoration: none;
+  font-family: Courier New, monospace;
+  text-align: center;
 }
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,12 @@
+<template>
+<title>just leo</title>
+        <div id="center">
+            <br>
+            <div id="avatar" style="background-image: url('https://i.imgur.com/8UhhUAL.jpg')">
+            </div></div>
+            <br>
+    <h1>hi, I'm Leo, I develop things and play</h1>
+
+    <center><a class="githubRedirect" href="https://github.com/leoo027"><i class="fa fa-github" style="font-size:50px;color:#FFF"></i><br>@leoo027</a></center>
+  <img src="https://cdn-icons-png.flaticon.com/512/733/733609.png" width="30"/>
+</template>
