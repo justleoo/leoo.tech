bdc910eb58f932195c3e8543de0072e5f4817112
leo <80543539+leoo027@users.noreply.github.com>
2021-11-23 20:42:57 -0300

    uuh

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <MainComponent/>
+  <Home/>
 </template>
 
 <script>
-import MainComponent from './components/MainComponent.vue'
+import MainComponent from './components/Home.vue'
 document.title = 'just leo' 
 export default {
   name: 'App',
   components: {
-    MainComponent
+    Home
   }
 }
 document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://i.imgur.com/3UePe8W.jpg')`
