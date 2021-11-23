b3ba98d343b03376e73877a8c96c7473b128c13b
leo <80543539+leoo027@users.noreply.github.com>
2021-11-23 17:34:52 -0300

    bug

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,5 @@ export default {
     MainComponent
   }
 }
-const github = () => window.location.href = "https://github.com/leoo027"
-const portfolio = () => window.location.href = "https://leooo-portfolio.vercel.app"
 document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://i.imgur.com/3UePe8W.jpg')`
 </script>
