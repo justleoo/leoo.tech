a1cb11667bb726a2f74319726310b1fa3fdacd4f
leo <80543539+leoo027@users.noreply.github.com>
2021-11-24 17:19:13 -0300

    vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Portfolio from './components/Portfolio.vue'
 import './assets/css/App.css'
-createApp(App).mount('#app')
+
+Vue.use(VueRouter)
+// router
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/portfolio',
+            components: Portfolio
+        }
+    ]
+})
+new Vue({
+    router, render: h => h(App),
+}).$mount('#app')
