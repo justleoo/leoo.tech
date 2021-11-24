4e621fff3677d5b8a65cf995bb2be001dd791cce
leo <80543539+leoo027@users.noreply.github.com>
2021-11-24 17:31:21 -0300

    i'm a dumb

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <Home/>
+  <router-view/>
 </template>
 
 <script>
-import Home from './components/Home.vue'
 document.title = 'just leo' 
 export default {
   name: 'App',
-  components: {
-    Home
-  }
 }
-document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://i.imgur.com/3UePe8W.jpg')`
 </script>
+
+<style>
+  body {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://i.imgur.com/3UePe8W.jpg');
+  }
+</style>
