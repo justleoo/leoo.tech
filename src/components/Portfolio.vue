6471c87065722bf94f7f2df0e19c443d9f9c5625
leo <80543539+leoo027@users.noreply.github.com>
2021-11-24 17:19:30 -0300

    vue routeeeer

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,12 @@
+<template>
+<title>just leo</title>
+        <div id="center">
+            <br>
+            <div id="avatar" style="background-image: url('https://cdn.discordapp.com/attachments/910257275120271390/910633584820375562/dae2a7831cc58f448310be1898f4c1b6.jpg')">
+            </div></div>
+            <br>
+    <h1>hi, I'm leonardo, call me leo.</h1>
+  <h3> I develop random things and I like to play</h3>
+  <h2>how did i get started on programming? I found something interesting, and I became more and more interested, with projects, and other things...</h2>
+  <h2>what do i like to do in my free time? well, i really like playing minecraft and other games, i love listening to music, mainly brazilian (i'm brazilian lol)</h2>
+</template>
