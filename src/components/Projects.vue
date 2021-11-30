16a49da1c8a0c972d1f9b69ec3272263cb217cec
leo <80543539+leoo027@users.noreply.github.com>
2021-11-30 19:35:09 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaw

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,5 @@
 <template>
   <h1>projects</h1>
-  
-  <div id="container">
          <div id="p">
             <div id="avatar" style="background-image: url('https://cdn.discordapp.com/attachments/893270199304880229/915360340559151154/514c6cc2bb8ab32548a409f04648d195.png')">
             </div>
@@ -15,5 +13,4 @@
                     just a simple api focused on images, ex: animals, wallpapers, and more.
                   </div>
               </div>
-           </div>
 </template>
