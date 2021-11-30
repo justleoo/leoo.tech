28d337de56ecaa0072ba11f706714b56a4ecf78b
leo <80543539+leoo027@users.noreply.github.com>
2021-11-30 18:55:24 -0300

    ye?

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>hello projects</h1>
+  <h1>projects</h1>
+  
+  <div id="container">
+         <div id="p">
+            <div id="avatar" style="background-image: url('https://cdn.discordapp.com/attachments/893270199304880229/915360340559151154/514c6cc2bb8ab32548a409f04648d195.png')">
+            </div>
+            <div id="desc_container">
+                <br>
+                <div id="name">
+                    torixApi
+                </div>
+                <div id="description">
+                    just a simple api focused on images, ex: animals, wallpapers, and more.
+                </div>
+            </div>
+        </div>
 </template>
