99a1a3084af3b20ca5623c376a8e2caa97d849b7
leo <80543539+leoo027@users.noreply.github.com>
2021-11-30 19:54:02 -0300

    http otters project added

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -13,4 +13,17 @@
                     just a simple api focused on images, ex: animals, wallpapers, and more.
                   </div>
               </div>
+              <div id="p">
+            <div id="avatar" style="background-image: url('https://media.discordapp.net/attachments/910257275120271390/910633584602251315/5f04884d11d7342b7ab2dd6a0896e7d5.jpg?width=271&height=270')">
+            </div>
+            <br>
+            <div id="desc_container">
+            </div>
+                <div id="name">
+                    HTTP Otters
+                </div>
+                <div id="description">
+                    http otters is a simple but cool project. developed by young Brazilians, who love programming, based on http.cat
+                  </div>
+              </div>
 </template>
